23864ee5dba744503f031fb91aa910d98dc380e1
Monica R <mlisareyes@gmail.com>
2019-10-06 17:02:12 +0000

    Automatically backed up by Learn

--- lib/concerns/music_library_controller.rb
+++ lib/concerns/music_library_controller.rb
@@ -60,7 +60,8 @@ class MusicLibraryController
     #songs = Song.all
     #songs.each.with_index(1) {|song, index| puts "Playing #{song.name} by #{song.artist.name}"}
     if user_input.between?(1, Song.all.size)
-    song = Song.all.sort {|a, b| a.name <=> b.name}
+      song = Song.all.sort {|a, b| a.name <=> b.name}
+
 
   end
 

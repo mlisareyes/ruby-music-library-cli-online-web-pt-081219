dbc12bd56ec9dfb485d5a2723efcf7aed061215c
Monica R <mlisareyes@gmail.com>
2019-10-06 17:37:43 +0000

    Automatically backed up by Learn

--- lib/concerns/music_library_controller.rb
+++ lib/concerns/music_library_controller.rb
@@ -23,14 +23,18 @@ class MusicLibraryController
 
     if user_input == "list songs"
       list_songs
-    elsif user_input == "list artists"
-      list_artists
-    elsif user_input == "list genres"
-      list_genres
-    elsif user_input == "list artist"
-      list_songs_by_artist
-    elsif user_input == "list genre"
-      list_songs_by_genre
+    elsif
+      user_input == "list artists"
+        list_artists
+    elsif
+      user_input == "list genres"
+        list_genres
+    elsif
+      user_input == "list artist"
+        list_songs_by_artist
+    elsif
+      user_input == "list genre"
+        list_songs_by_genre
     else user_input == "play song"
       play_song
     end

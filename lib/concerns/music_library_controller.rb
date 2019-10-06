3eaa0493fe56c84696b1044afaa1c789677c8d82
Monica R <mlisareyes@gmail.com>
2019-10-06 17:44:22 +0000

    Automatically backed up by Learn

--- lib/concerns/music_library_controller.rb
+++ lib/concerns/music_library_controller.rb
@@ -32,7 +32,7 @@ class MusicLibraryController
       list_songs_by_artist
     when "list genre"
       list_songs_by_genre
-    when  "play song"
+    when "play song"
       play_song
     end
   end

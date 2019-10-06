81f59f492a5f7e6ae3d1736be7da225fdb531eaf
Monica R <mlisareyes@gmail.com>
2019-10-06 16:46:37 +0000

    Automatically backed up by Learn

--- lib/concerns/music_library_controller.rb
+++ lib/concerns/music_library_controller.rb
@@ -57,7 +57,7 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     user_input = gets.chomp
-    songs = Song.all[index]
+    songs = Song.all
     songs.each.with_index(1) {|song, index| puts "Playing #{song.name} by #{song.artist.name}"}
   end
 

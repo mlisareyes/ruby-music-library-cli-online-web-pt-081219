c1efbe7aa3c7b12aa5f94264d8e88d692a119b16
Monica R <mlisareyes@gmail.com>
2019-10-06 17:40:16 +0000

    Automatically backed up by Learn

--- lib/concerns/music_library_controller.rb
+++ lib/concerns/music_library_controller.rb
@@ -21,21 +21,18 @@ class MusicLibraryController
 
     user_input = gets.chomp
 
-    if user_input == "list songs"
+    case
+    when user_input == "list songs"
       list_songs
-    elsif
-      user_input == "list artists"
-        list_artists
-    elsif
-      user_input == "list genres"
-        list_genres
-    elsif
-      user_input == "list artist"
-        list_songs_by_artist
-    elsif
-      user_input == "list genre"
-        list_songs_by_genre
-    else user_input == "play song"
+    when user_input == "list artists"
+      ist_artists
+    when user_input == "list genres"
+      list_genres
+    when user_input == "list artist"
+      list_songs_by_artist
+    when user_input == "list genre"
+      list_songs_by_genre
+    when user_input == "play song"
       play_song
     end
   end

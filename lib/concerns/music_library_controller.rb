796a7f0dffe979f0cab30423d79c856393c7bdcc
Monica R <mlisareyes@gmail.com>
2019-10-06 17:46:18 +0000

    Automatically backed up by Learn

--- lib/concerns/music_library_controller.rb
+++ lib/concerns/music_library_controller.rb
@@ -21,18 +21,19 @@ class MusicLibraryController
     user_input = gets.chomp
 
     case user_input
-    when "list songs"
-      list_songs
-    when "list artists"
-      list_artists
-    when "list genres"
-      list_genres
-    when "list artist"
-      list_songs_by_artist
-    when "list genre"
-      list_songs_by_genre
-    when "play song"
-      play_song
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
     end
   end
 
